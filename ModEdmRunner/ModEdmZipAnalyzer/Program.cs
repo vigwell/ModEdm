bc2d0d73ee5c0b7acb279cb024122165452b3ce3
vigwell <134093446+vigwell@users.noreply.github.com>
2025-02-14 09:36:34 +0200

    fix

--- ModEdmRunner/ModEdmZipAnalyzer/Program.cs
+++ ModEdmRunner/ModEdmZipAnalyzer/Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
 using ModEdmZipAnalyzer;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
 
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = "ModEdmZipAnalyzerService";
+});
+LoggerProviderOptions.RegisterProviderOptions<EventLogSettings, EventLogLoggerProvider>(builder.Services);
+builder.Services.AddHostedService<Worker>();
+builder.Logging.AddConfiguration(
+    builder.Configuration.GetSection("Logging"));
 var host = builder.Build();
 host.Run();
+
